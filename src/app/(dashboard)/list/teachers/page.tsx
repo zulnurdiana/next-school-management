6d8381a505c0ae53886d17ed4fc759bc2de09096
Zulfikar Nurdiana <66067543+zulnurdiana@users.noreply.github.com>
2024-10-09 13:00:29 +0700

    ui: reuseable list

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,11 +1,102 @@
 import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
+import { role, teachersData } from "@/lib/data";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
+  type Teacher = {
+    id: number;
+    teacherId: string;
+    name: string;
+    email?: string;
+    photo: string;
+    phone: string;
+    subjects: string[];
+    classes: string[];
+    address: string;
+  };
+
+  const columns = [
+    {
+      header: "Info",
+      accessor: "info",
+    },
+    {
+      header: "Teacher ID",
+      accessor: "teacherId",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Subjects",
+      accessor: "subjects",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Classes",
+      accessor: "classes",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Phone",
+      accessor: "phone",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Address",
+      accessor: "address",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Actions",
+      accessor: "action",
+    },
+  ];
+
+  const renderRow = (item: Teacher) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 text-xs even:bg-slate-50 hover:bg-lamaPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <Image
+          src={item.photo}
+          alt={""}
+          height={40}
+          width={40}
+          className="md:hidden w-10 h-10 xl:block rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <h2 className="font-medium">{item.name}</h2>
+          <span className="text-xs text-gray-500">{item?.email}</span>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.teacherId}</td>
+      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
+      <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src={"/view.png"} alt={""} height={16} width={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaPurpleLight">
+              <Image src={"/delete.png"} alt={""} height={16} width={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
   return (
-    <div className="w-full bg-white rounded-md p-4 mt-0 flex-1">
+    <div className=" bg-white rounded-md p-4 mt-0 flex-1">
       {/* HEADER */}
       <div className="flex items-center justify-between">
         <h1 className="hidden md:block text-xl font-semibold">All Teachers</h1>
@@ -25,6 +116,7 @@ const page = () => {
         </div>
       </div>
       {/* LIST */}
+      <Table columns={columns} renderRow={renderRow} data={teachersData} />
 
       {/* PAGINATION */}
       <Pagination />
---
+++ src/components/Table.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Table = ({
+  columns,
+  renderRow,
+  data,
+}: {
+  columns: { header: string; accessor: string; className?: string }[];
+  renderRow: (item: any) => React.ReactNode;
+  data: any[];
+}) => {
+  return (
+    <table className="w-full mt-4">
+      <thead>
+        <tr className="text-gray-500 text-left text-xs">
+          {columns.map((column) => (
+            <td className={column.className} key={column.accessor}>
+              {column.header}
+            </td>
+          ))}
+        </tr>
+      </thead>
+      <tbody>{data.map((item) => renderRow(item))}</tbody>
+    </table>
+  );
+};
+
+export default Table;
