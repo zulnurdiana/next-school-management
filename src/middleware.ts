2f970219d850ad9c111c5c169a2435b04858b45f
Zulfikar Nurdiana <66067543+zulnurdiana@users.noreply.github.com>
2024-10-19 10:22:02 +0700

    feat: protect route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,27 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { routeAccessMap } from "./lib/settings";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+// const isProtectedRoute = createRouteMatcher(["/admin", "/teacher"]);
+
+const matchers = Object.keys(routeAccessMap).map((route) => ({
+  matcher: createRouteMatcher([route]),
+  allowedRoles: routeAccessMap[route],
+}));
+
+export default clerkMiddleware((auth, req) => {
+  // if (isProtectedRoute(req)) auth().protect()
+
+  const { sessionClaims } = auth();
+
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+
+  for (const { matcher, allowedRoles } of matchers) {
+    if (matcher(req) && !allowedRoles.includes(role!)) {
+      return NextResponse.redirect(new URL(`/${role}`, req.url));
+    }
+  }
+});
 
 export const config = {
   matcher: [
